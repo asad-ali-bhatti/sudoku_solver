1a43ff47ba8fdb12d5ff7056f16c8effcb379045
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-16 23:24:33 +0200

    adds coordinates to cell

--- lib/sudoku_solver/cell.rb
+++ lib/sudoku_solver/cell.rb
@@ -1,4 +1,14 @@
 class Cell
-  attr_accessor :x, :y, :value, :box_number
+  attr_accessor :x, :y, :value
+
+  def initialize(x, y, value)
+    self.x = x
+    self.y = y
+    self.value = value
+  end
+
+  def box_coords
+    @box_coords ||= { x: x / 3, y: y / 3 }
+  end
 end
 
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,12 +1,48 @@
 require_relative   'rspec_helper'
 
+=begin
+  Game board is designed in following format
+  each cell format is [x,y, value]
+
+ [0,0, ?] [0,1, ?] .... [0,8, ?]
+ [1,0, ?] [1,1, ?] .... [1,8, ?]
+ ....     ....          ....
+ ....     ....          ....
+ ....     ....          ....
+ [8,0, ?] [8,1, ?] .... [8,8, ?]
+
+ Also there are 6 boxes. each having 9 cells
+
+ [0,0] [0,1] [0.2]
+ [1,0] [1,1] [1,2]
+ [2,0] [2,1] [2.2]
+=end
+
 RSpec.describe Cell do
+  let(:x) { 2 }
+  let(:y) { 3 }
+
+  subject { Cell.new(x,y, 9) }
 
   describe 'Attributes' do
+
     it { should respond_to :x }
     it { should respond_to :y }
     it { should respond_to :value }
-    it { should respond_to :box_number }
+  end
+
+  describe '#box_coords' do
+
+    it { should respond_to :box_coords }
+
+    it 'should return hash with box location' do
+      expect(subject.box_coords.keys).to be_eql([:x, :y])
+    end
+
+    it 'should calculate coords of each cell' do
+      expect(subject.box_coords[:x]).to be_eql(0) # 2/3 == 0
+      expect(subject.box_coords[:y]).to be_eql(1) # 3/3 == 1
+    end
   end
 end
 
